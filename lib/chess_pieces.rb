5214122f796ca9770e65e9239ac8bb6e6f113941
kregor <kregorjeager@gmail.com>
2025-12-14 18:27:28 +0800

    Add method to pass test for class Pawn

--- lib/chess_pieces.rb
+++ lib/chess_pieces.rb
@@ -9,6 +9,20 @@ class Pawn
     @team = team
     @character = team == 'white' ? "\u2659" : "\u265F"
   end
+
+  def available_steps(pos)
+    polarity = @team == 'white' ? -1 : 1
+    # when next move is outside of board
+    return nil unless (pos[0] + (1 * polarity)).between?(0, 7)
+
+    # when pawn is making it's first move it returns two available steps
+    y = pos[0]
+    x = pos[1]
+    return [[y + 1 * polarity, x], [y + 2 * polarity, x]] if [1, 6].include?(y)
+
+    # only return 1 availble steps if not
+    [[y + 1 * polarity, x]]
+  end
 end
 
 # Rook
