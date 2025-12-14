98d15499ae31f3654f17e786c9f2ff494c6be5a7
kregor <kregorjeager@gmail.com>
2025-12-14 16:23:44 +0800

    Misc test for chess pieces to check for error

--- testing/chess_board_test.rb
+++ testing/chess_board_test.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: false
 
-require_relative '../lib/ChessBoard'
+require_relative '../lib/chess_board'
+require_relative '../lib/chess_pieces'
 
 ChessBoard.new.print_board
+
+pieces = [Pawn, Rook, Horse, Bishop, King, Queen]
+
+pieces.each do |piece|
+  a = piece.new('black')
+  puts "#{a.role}:#{a.character} "
+end
+
+pieces.each do |piece|
+  a = piece.new('white')
+  puts "#{a.role}:#{a.character} "
+end
