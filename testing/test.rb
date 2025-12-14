35d2f2a5381d5dadc60b8747770d921ff03285dd
kregor <kregorjeager@gmail.com>
2025-12-14 15:02:29 +0800

    Misc testing

--- testing/test.rb
+++ testing/test.rb
@@ -18,7 +18,32 @@ class Inner < Outer
 end
 
 x = Inner.new('Al')
-p x.test_var
+# p x.test_var
 x.update_var('Alma')
-p x.test_var
+# p x.test_var
+# End   +++++++++++++++++++++++
+
+# When superclass is able to use subclass methods
+# Start +++++++++++++++++++++++
+# nil
+class Outer1
+  attr_reader :hey
+
+  def initialize
+    @hey = 'hello'
+    @value = 'nil'
+    p @value
+  end
+end
+
+# nil
+class Inner1 < Outer1
+  def initialize
+    super
+    @value = 'something'
+    p @value
+  end
+end
+
+p Inner1.new.hey
 # End   +++++++++++++++++++++++
