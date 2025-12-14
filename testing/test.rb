8acca134e1f133ee1dc3bf1752e84eb8213df06c
kregor <kregorjeager@gmail.com>
2025-12-14 13:56:16 +0800

    Misc testing

--- testing/test.rb
+++ testing/test.rb
@@ -1,1 +1,24 @@
 # frozen_string_literal: false
+
+# when subclass is able to change a instance_variable from superclass
+# Start +++++++++++++++++++++++
+class Outer
+  attr_accessor :test_var
+
+  def initialize(string)
+    @test_var = string
+  end
+end
+
+# nil
+class Inner < Outer
+  def update_var(string)
+    @test_var = string
+  end
+end
+
+x = Inner.new('Al')
+p x.test_var
+x.update_var('Alma')
+p x.test_var
+# End   +++++++++++++++++++++++
