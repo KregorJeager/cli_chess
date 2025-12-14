57c187a3e0c1a2e5dbbb9b5e096fde9704525540
kregor <kregorjeager@gmail.com>
2025-12-14 16:37:17 +0800

    Update print method to access class characters

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -3,12 +3,17 @@
 # board
 class ChessBoard
   def initialize
-    @board = Array.new(8) { Array.new(8) { '[ ]' } }
+    @board = Array.new(8) { Array.new(8) { nil } }
   end
 
   def print_board
-    @board.each do |i|
-      puts i.join('')
+    puts '  0   1   2   3   4   5   6   7'
+    @board.each_index do |i|
+      line = []
+      @board[i].each do |j|
+        line << (j.nil? ? '[  ]' : j.character)
+      end
+      puts "#{i}#{line.join('')}"
     end
   end
 end
