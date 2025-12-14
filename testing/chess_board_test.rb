60d8889a2c6ca9466bcc90c4dd8f2bed80e8a5f2
kregor <kregorjeager@gmail.com>
2025-12-14 18:30:04 +0800

    Configure rspec and misc testing

--- testing/chess_board_test.rb
+++ testing/chess_board_test.rb
@@ -16,3 +16,13 @@ pieces.each do |piece|
   a = piece.new('white')
   puts "#{a.role}:#{a.character} "
 end
+
+hash = [{ Pawn => [1, 2] }]
+hash[0].each_key { |key| p key.new('white') }
+
+a = ChessBoard.new
+a.print_board
+a.set_to_default
+a.print_board
+a.move([1, 1], [3, 1])
+a.print_board
