fb3e7df5762cedf1dc07aef365805e1ac4b62c15
kregor <kregorjeager@gmail.com>
2025-12-18 20:02:13 +0800

    Add spec for when a piece is moved in straights

--- spec/chess_cop_module_spec.rb
+++ spec/chess_cop_module_spec.rb
@@ -1,1 +1,51 @@
 # frozen_string_literal:false
+
+describe ChessCop do
+  subject(:cop) { Class.new { include ChessCop } }
+  let(:board) { Array.new(8) { Array.new(8) { nil } } }
+
+  # A method that calculates all y,x positions that a piece will travel and return
+  #  them in an array.
+  describe '#get_path_positions' do
+    describe 'straight movevement' do
+      context 'when moving to the right' do
+        xit 'return an array of y,x positions from left to right' do
+          current_position = [1, 1]
+          new_postion = [1, 4]
+          positions_along_the_way = [[1, 2], [1, 3]]
+          expect(cop.get_path_pos(current_position, new_postion))
+            .to eq(positions_along_the_way)
+        end
+      end
+
+      context 'when moving to the left' do
+        it 'returns array of positions from right to left' do
+          current_position = [6, 6]
+          new_postion = [6, 1]
+          positions_along_the_way = [[6, 5], [6, 4], [6, 3], [6, 2]]
+          expect(cop.get_path_pos(current_position, new_postion))
+            .to eq(positions_along_the_way)
+        end
+      end
+      context 'when moving upward' do
+        it 'returns array of positions from bottom to top' do
+          current_position = [4, 5]
+          new_postion = [4, 8]
+          positions_along_the_way = [[4, 6], [4, 7]]
+          expect(cop.get_path_pos(current_position, new_postion))
+            .to eq(positions_along_the_way)
+        end
+      end
+
+      context 'when moving downward' do
+        it 'returns array of positions from top to bottom' do
+          current_position = [7, 7]
+          new_postion = [7, 4]
+          positions_along_the_way = [[7, 6], [7, 5]]
+          expect(cop.get_path_pos(current_position, new_postion))
+            .to eq(positions_along_the_way)
+        end
+      end
+    end
+  end
+end
