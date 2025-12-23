45fb95b0301adb1c73664dd0b9791a7b851dc384
kregor <kregorjeager@gmail.com>
2025-12-23 20:41:00 +0800

    Add method to determine the orientation of move

--- lib/chess_cop_module.rb
+++ lib/chess_cop_module.rb
@@ -1,1 +1,15 @@
 # frozen_string_literal: false
+
+module ChessCop
+  def get_path_positions(curr, new)
+    orientation = diagonal?(curr, new)
+
+    orientation ? get_diagonal(curr, new) : get_straight(curr, new)
+  end
+
+  def diagonal?(curr, new)
+    return true if curr[0] != new[0] && curr[0] != new[1]
+
+    false
+  end
+end
