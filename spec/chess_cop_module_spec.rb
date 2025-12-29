c2e30305d545009555f54a2dac0c4eaba4748e77
kregor <kregorjeager@gmail.com>
2025-12-29 19:46:20 +0800

    Fix output expectation and include the last pos
    Some test has typo in their in thier expected outp
    ut. Also updated to include the last or new pos
    
    The pos update is so we can also confirm if the
    landing pos is also empty

--- spec/chess_cop_module_spec.rb
+++ spec/chess_cop_module_spec.rb
@@ -2,7 +2,7 @@
 
 require_relative '../lib/chess_cop_module'
 describe ChessCop do
-  subject(:cop) { Class.new { include ChessCop } }
+  subject(:cop) { Class.new { extend ChessCop } }
   let(:board) { Array.new(8) { Array.new(8) { nil } } }
 
   # A method that calculates all y,x positions that a piece will travel and return
@@ -13,36 +13,36 @@ describe ChessCop do
         it 'return an array of y,x positions from left to right' do
           current_position = [1, 1]
           new_postion = [1, 4]
-          positions_along_the_way = [[1, 2], [1, 3]]
+          positions_along_the_way = [[1, 2], [1, 3], [1, 4]]
           expect(cop.get_path_pos(current_position, new_postion))
             .to eq(positions_along_the_way)
         end
       end
 
       context 'when moving to the left' do
-        xit 'returns array of positions from right to left' do
+        it 'returns array of positions from right to left' do
           current_position = [6, 6]
           new_postion = [6, 1]
-          positions_along_the_way = [[6, 5], [6, 4], [6, 3], [6, 2]]
+          positions_along_the_way = [[6, 5], [6, 4], [6, 3], [6, 2], [6, 1]]
           expect(cop.get_path_pos(current_position, new_postion))
             .to eq(positions_along_the_way)
         end
       end
       context 'when moving upward' do
-        xit 'returns array of positions from bottom to top' do
+        it 'returns array of positions from bottom to top' do
           current_position = [4, 5]
           new_postion = [4, 8]
-          positions_along_the_way = [[4, 6], [4, 7]]
+          positions_along_the_way = [[4, 6], [4, 7], [4, 8]]
           expect(cop.get_path_pos(current_position, new_postion))
             .to eq(positions_along_the_way)
         end
       end
 
       context 'when moving downward' do
-        xit 'returns array of positions from top to bottom' do
+        it 'returns array of positions from top to bottom' do
           current_position = [7, 7]
           new_postion = [7, 4]
-          positions_along_the_way = [[7, 6], [7, 5]]
+          positions_along_the_way = [[7, 6], [7, 5], [7, 4]]
           expect(cop.get_path_pos(current_position, new_postion))
             .to eq(positions_along_the_way)
         end
