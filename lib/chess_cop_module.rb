268fbb6a5d2825e1befaeddb7bae68967c4fa50b
kregor <kregorjeager@gmail.com>
2025-12-31 11:48:18 +0800

    Refactor straight move algorithm

--- lib/chess_cop_module.rb
+++ lib/chess_cop_module.rb
@@ -6,19 +6,7 @@ module ChessCop
     return curr if curr == new
     return nil if new[0] > 7 || new[0].negative? || new[1] > 7 || new[1].negative?
 
-    if curr[0] == new[0] || curr[1] == new[1]
-      axis = curr[0] == new[0] ? 1 : 0 # find the axis where it moves
-      polarity = curr[axis] > new[axis] ? -1 : 1 # find direction of the move
-      path = [] # array of positions the move passes thru
-      count = 0
-      temp = curr
-      until temp[axis] == new[axis]
-        count += 1
-        temp = axis.zero? ? [curr[0] + count * polarity, curr[1]] : [curr[0], curr[1] + count * polarity]
-        path << temp
-      end
-      return path
-    end
+    return straight_move(curr, new) if curr[0] == new[0] || curr[1] == new[1]
 
     x_polarity = curr[1] > new[1] ? -1 : 1
     y_polarity = curr[0] > new[0] ? -1 : 1
@@ -35,5 +23,19 @@ module ChessCop
       break if new == [curr[0] + count * y_polarity, curr[1] + count * x_polarity]
     end
     diag_temp
+  end
+
+  def straight_move(curr, new)
+    axis = curr[0] == new[0] ? 1 : 0 # find the axis where it moves
+    polarity = curr[axis] > new[axis] ? -1 : 1 # find direction of the move
+    path = [] # array of positions the move passes thru
+    count = 0
+    temp = curr
+    until temp[axis] == new[axis]
+      count += 1
+      temp = axis.zero? ? [curr[0] + count * polarity, curr[1]] : [curr[0], curr[1] + count * polarity]
+      path << temp
+    end
+    path
   end
 end
