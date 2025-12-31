0bb4b5e0e2937706343a0e6955f3179ad8f6e241
kregor <kregorjeager@gmail.com>
2026-01-01 00:10:18 +0800

    Add test for filter_input

---
+++ spec/game_round_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: false
+
+require './lib/game_round'
+
+describe GameRound do
+  describe '#filter_input' do
+    subject(:game) { described_class.new }
+    context 'types of input' do
+      it 'returns formated array when given postions' do
+        expect(game.filter_input('1,1|1,5')).to eq([[1, 1], [1, 5]])
+      end
+      it 'return string when input does not match a coordinate' do
+        expect(game.filter_input('save')).to eq('save')
+      end
+    end
+
+    describe '#make_move' do
+      it '' do
+      end
+    end
+  end
+end
